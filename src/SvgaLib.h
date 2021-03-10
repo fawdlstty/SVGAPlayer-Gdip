e3b104fcc4f2c79970d773ee61d46bdb8fe9c4b3
fawdlstty <f@fawdlstty.com>
2021-03-10 21:37:04 +0800

    init

---
+++ src/SvgaLib.h
@@ -0,0 +1,26 @@
+﻿#ifndef __SVGALIB_H__
+#define __SVGALIB_H__
+
+#include <cstdint>
+#include <functional>
+#include <memory>
+#include <string>
+
+
+
+namespace SvgaLib {
+	class SvgaObject: public std::enable_shared_from_this<SvgaObject> {
+		SvgaObject () = default;
+
+	public:
+		~SvgaObject () { Stop (); }
+		void StartPlay (std::function<void ()>);
+		void Stop ();
+		bool IsPlaying ();
+
+		static std::shared_ptr<SvgaObject> LoadFromMemory (const uint8_t* _data, size_t _size);
+		static std::shared_ptr<SvgaObject> LoadFromFile (const std::string _path);
+	};
+}
+
+#endif //__SVGALIB_H__
---
+++ src/dllmain.cpp
@@ -0,0 +1,9 @@
+#ifdef _WIN32
+
+#include <windows.h>
+
+BOOL CALLBACK DllMain (HINSTANCE inst, DWORD reason, LPVOID reserved) {
+	return TRUE;
+}
+
+#endif
